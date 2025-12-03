08caa332dea82da9185ed3b51175ee13ba6ab73e
Himmat Singh <himmsrathore@hotmail.com>
2025-12-03 10:35:59 +0530

    Update app to sum random numbers

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello"
+    num1 = random.randint(1, 100)
+    num2 = random.randint(1, 100)
+    total = num1 + num2
+    return f"Number 1: {num1}, Number 2: {num2}, Sum: {total}"
 
 if __name__ == '__main__':
     app.run(debug=True)
