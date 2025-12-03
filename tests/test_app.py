1f906a2b54ba88e8e9409d0a2332bc1babc82adb
Himmat Singh <himmsrathore@hotmail.com>
2025-12-03 10:40:49 +0530

    Setup CI/CD with GitHub Actions

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_hello(client):
+    rv = client.get('/')
+    assert rv.status_code == 200
+    assert b"Number 1:" in rv.data
+    assert b"Sum:" in rv.data
